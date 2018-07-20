8b6e3de1cf1e10067788a62ecaccad26cfbee604
pszymczyk <pawel.szymczyk90@gmail.com>
2018-07-20 07:11:39 +0200

    fix typo

--- src/main/java/com/pszymczyk/gearbox/GearCalculator.java
+++ src/main/java/com/pszymczyk/gearbox/GearCalculator.java
@@ -2,5 +2,5 @@ package com.pszymczyk.gearbox;
 
 public interface GearCalculator {
 
-    int calculate(int curentRPM, double threshold, double force);
+    int calculate(int currentRPM, double threshold, double force);
 }
--- src/main/java/com/pszymczyk/gearbox/SportGearCalculation.java
+++ src/main/java/com/pszymczyk/gearbox/SportGearCalculation.java
@@ -2,7 +2,7 @@ package com.pszymczyk.gearbox;
 
 public class SportGearCalculation implements GearCalculator {
     @Override
-    public int calculate(int curentRPM, double threshold, double force) {
+    public int calculate(int currentRPM, double threshold, double force) {
         return 0;
     }
 }
