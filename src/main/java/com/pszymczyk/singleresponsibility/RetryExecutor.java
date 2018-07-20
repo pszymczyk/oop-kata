4133860e999904791471f7e930d634c3d43143d6
pszymczyk <pawel.szymczyk90@gmail.com>
2018-07-20 16:32:45 +0200

    improve retry executor

--- src/main/java/com/pszymczyk/singleresponsibility/RetryExecutor.java
+++ src/main/java/com/pszymczyk/singleresponsibility/RetryExecutor.java
@@ -4,6 +4,31 @@ package com.pszymczyk.singleresponsibility;
  * @author pawel szymczyk
  */
 class RetryExecutor {
-    public static void execute(int maxTries, Exception exc, Runnable consumer) {
+
+    public void run() {
+
     }
+
+    static class Builder {
+        static Builder retryExecutor() {
+            return new Builder();
+        }
+
+        Builder withRetryCount(int retryCount) {
+            return this;
+        }
+
+        Builder retryOnException(Class<? extends Throwable> exception) {
+            return this;
+        }
+
+        Builder withCustomFinalException(RuntimeException runtimeException) {
+            return this;
+        }
+
+        RetryExecutor build(Runnable runnable) {
+            return new RetryExecutor();
+        }
+    }
+
 }
