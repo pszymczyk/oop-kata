67fb247175a2503d104d317941501ef61b052a5a
pszymczyk <pawel.szymczyk90@gmail.com>
2018-10-06 12:12:02 +0200

    remove implementation

--- src/main/java/com/pszymczyk/singleresponsibility/ItemBoughtEventHandler.java
+++ src/main/java/com/pszymczyk/singleresponsibility/ItemBoughtEventHandler.java
@@ -1,20 +1,46 @@
 package com.pszymczyk.singleresponsibility;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.event.EventListener;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author pawel szymczyk
  */
 class ItemBoughtEventHandler {
 
-    private final ItemBoughtRepository itemBoughtRepository;
+    private static final int MAX_TRIES = 3;
 
-    public ItemBoughtEventHandler(ItemBoughtRepository itemBoughtRepository) {
-        this.itemBoughtRepository = itemBoughtRepository;
+    private final String url;
+    private final RestTemplate restTemplate;
+
+    public ItemBoughtEventHandler(String url) {
+        this.url = url;
+        this.restTemplate = restTemplate();
     }
 
     @EventListener
     public void handle(ItemBought itemBought) {
-        itemBoughtRepository.save(itemBought);
+        int count = 0;
+
+        while (true) {
+            try {
+                restTemplate.postForEntity(url, itemBought, Void.class);
+                return;
+            } catch (HttpClientErrorException | HttpServerErrorException ex) {
+                if (++count == MAX_TRIES) {
+                    throw new RuntimeException("Cannot send ItemBought event");
+                }
+            }
+        }
+    }
+
+    private RestTemplate restTemplate() {
+        return new RestTemplateBuilder()
+                .setConnectTimeout(100)
+                .setReadTimeout(500)
+                .build();
     }
 }
